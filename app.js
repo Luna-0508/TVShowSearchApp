88f311d8167060b44de8a0212c526a7e4f22b568
Natalia Łukaszka <lukaszkanatalia94@gmail.com>
2022-09-02 20:39:22 +0200

    Connect to TV search API, made function async

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const form = document.querySelector("#searchForm");
 
-form.addEventListener("submit", function (e) {
+form.addEventListener("submit", async function (e) {
   e.preventDefault();
   const searchTerm = form.elements.query.value;
+  const res = await axios.get(
+    `https://api.tvmaze.com/search/shows?q=${searchTerm}`
+  );
 });
