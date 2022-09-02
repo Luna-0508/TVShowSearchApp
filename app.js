d23c581695a147e1f2e1e8f0f5e4ce21596cda85
Natalia Łukaszka <lukaszkanatalia94@gmail.com>
2022-09-02 20:36:08 +0200

    Add search var, add script

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const form = document.querySelector("#searchForm");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  console.log("SUBMITED!");
+  const searchTerm = form.elements.query.value;
 });
