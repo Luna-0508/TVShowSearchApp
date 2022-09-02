d38e4a3ad0db6afd46636198376458929a763032
Natalia Łukaszka <lukaszkanatalia94@gmail.com>
2022-09-02 20:43:15 +0200

    create img var, made it appear to page

--- app.js
+++ app.js
@@ -6,4 +6,8 @@ form.addEventListener("submit", async function (e) {
   const res = await axios.get(
     `https://api.tvmaze.com/search/shows?q=${searchTerm}`
   );
+
+  const img = document.createElement("img");
+  img.src = res.data[0].show.image.medium;
+  document.body.append(img);
 });
