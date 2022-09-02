5be9f640f472d8b3dea61e387e461b8ad8a22363
Natalia Łukaszka <lukaszkanatalia94@gmail.com>
2022-09-02 20:54:25 +0200

    Create object for query, update API string

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const form = document.querySelector("#searchForm");
 form.addEventListener("submit", async function (e) {
   e.preventDefault();
   const searchTerm = form.elements.query.value;
-  const res = await axios.get(
-    `https://api.tvmaze.com/search/shows?q=${searchTerm}`
-  );
+  const config = { params: { q: searchTerm } };
+  const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
   displayImg(res.data);
+  form.elements.query.value = "";
 });
 
 const displayImg = shows => {
